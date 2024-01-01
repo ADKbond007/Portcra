7e8d93c92f6c63e8f5c4e2e92fa3015a21034064
Abhishek kr <abhishekdkinghasrisen@gmail.com>
2024-01-01 15:00:38 +0530

    Fixing active link issue navbar

--- src/components/GotoTop.js
+++ src/components/GotoTop.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
 import "../scss/gototop.scss";
 const GotoTop = ({ topBtn }) => {
-    return (
-        <div className='gototop-cont' style={{ display: topBtn }}>
-            <a className='gototop-link' href="#mydetails">
-                <Button><i className="fa-solid fa-jet-fighter-up"></i></Button>
-            </a>
-        </div>
-    );
+  const chandler = () => {
+    document
+      .querySelectorAll(".nav-link")
+      .forEach((e) => e.classList.remove("active"));
+  };
+  return (
+    <div className="gototop-cont" style={{ display: topBtn }}>
+      <a className="gototop-link" href="#mydetails">
+        <Button className="gototop-btn" onClick={chandler}>
+          <i className="fa-solid fa-jet-fighter-up"></i>
+        </Button>
+      </a>
+    </div>
+  );
 };
 
 export default GotoTop;
