8ef7789d9b59010a0800c748fee86df80a57bfa6
ADKbond007 <98117209+ADKbond007@users.noreply.github.com>
2025-10-01 19:44:25 +0530

    Update Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -18,7 +18,7 @@ const Details = () => {
             <span className="profile-name-black">Front End</span> Web Developer
           </span>
           <a
-            href="https://drive.google.com/file/d/1ij495l7_L7NtKwf4EzA1UMK7zML5RXnI/view?usp=drive_link"
+            href="https://drive.google.com/file/d/10FsFewSC2TbJ804dJC34oQwnb_5O-qQ_/view?usp=drive_link"
             target="_new"
           >
             <button className="resume-btn">Resume</button>
